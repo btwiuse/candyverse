8cd5baf72da2b4ae85d75141b2720be69102f902
navigaid <navigaid@gmail.com>
2022-08-05 13:06:34 +0800

    Zodiac World

--- src/Home.tsx
+++ src/Home.tsx
@@ -650,7 +650,7 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>Phantom Fruits</h2>
+                            <h2>Zodiac World</h2>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
